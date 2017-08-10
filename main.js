f7fbc12b1c723c57b75372461d04e7e00d1bd0b5
Brandon Aiton <brandonaiton@icloud.com>
2017-08-10 17:31:48 -0400

    added some styling so I can better see the problem'

--- main.js
+++ main.js
@@ -6,31 +6,49 @@
 let musicPlayerArea = document.querySelector(".player");
 let actualPlayer = document.querySelector(".music-player");
 let searchArea = document.querySelector(".search");
-let actualSearchForm= document.querySelector(".search-form");
-let resultsArea = document.querySelector(".results");
+let actualSearchForm = document.querySelector(".search-form");
+let resultsArea = document.querySelector(".resultWrapper");
 let submitButton = document.getElementById("button");
 
-let getSearch =  encodeURIComponent("");
+let getSearch = encodeURIComponent("");
 
 let baseURL = "https://itunes.apple.com/search?term="
 
 // 2. Create your `submit` event for getting the user's search term
 
-actualSearchForm.addEventListener("submit", function(event){
+actualSearchForm.addEventListener("submit", function(event) {
   event.preventDefault()
   console.log("clicked");
   getSearch = event.target.querySelector("input[name='search']").value
 
-// https://itunes.apple.com/search?term=jack+johnson&limit=25
-// 3. Create your `fetch` request that is called after a submission
-fetch(baseURL + getSearch + "&limit=25")
-  .then(function(response){
-    response.json()
-    // console.log(response.status);
-    .then(function(data){
-      console.log(data.results);
+
+
+  // 3. Create your `fetch` request that is called after a submission
+  fetch(baseURL + getSearch + "&limit=15")
+    .then(function(response) {
+      response.json()
+        // console.log(response.status);
+        .then(function(data) {
+          console.log(data.results);
+          let songs = data.results
+          // 4. Create a way to append the fetch results to your page
+          for (i = 0; i < songs.length; i++) {
+            function myHtml(data) {
+              let song = songs[i];
+              let results = `
+                <div class="song">
+                  <img src="${song.artworkUrl100}" alt="Album Artwork">
+                  <p class="track">${song.trackName}</p>
+                  <p class="band">${song.artistName}</p>
+                </div>
+            `
+              return results;
+            }
+            let addingSongs = myHtml(data);
+            resultsArea.innerHTML += addingSongs;
+          }
+          // 5. Create a way to listen for a click that will play the song in the audio play
+        })
     })
-  })
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
+
 })
